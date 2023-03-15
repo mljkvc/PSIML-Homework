3bff950406fca6fd7b841c2a3a22b81c3c6a0b74
Marko <60828160+mljkvc@users.noreply.github.com>
2023-03-15 11:44:57 +0100

    Add files via upload

--- 3.py
+++ 3.py
@@ -2,17 +2,19 @@ import numpy as np
 from math import sqrt, pi, cos, sin
 from PIL import Image, ImageFilter, ImageDraw
 import PIL.PngImagePlugin as png
+import time
+from collections import defaultdict
 
-#task 1 radi super
+boje = {
+    (255, 255, 0): 'Y',
+    (0, 0, 255): 'B',
+    (0, 0, 0): 'K',
+    (0, 255, 0): 'G',
+    (255, 0, 0): 'R'
+}
+
+#task 1 radi
 def task1(img):
-
-    boje = {
-        (255, 255, 0): 'Y',
-        (0, 0, 255): 'B',
-        (0, 0, 0): 'K',
-        (0, 255, 0): 'G',
-        (255, 0, 0): 'R'
-    }
 
     brojac = {
         'Y': 0,
@@ -28,55 +30,121 @@ def task1(img):
 
     for n, m in brojac.items():
         print(n, m)
+#---------------------------
 
-#task2 ne radi bas sjajno
-def hough(image: png.PngImageFile, radius, threshold):
-    #u grayscale
-    gray = image.convert('L')
+#task2 -----------------------------------------------
+
+#vraca najblizi krug
+def nadji(krugovi, boja1, boja2):
+        
+    min_rast = 800
+    min_krug = 0
+    for krug1 in krugovi:
+        for krug2 in krugovi:
+            if krug1[0] == boja1 and krug2[0] == boja2:
+                rast = sqrt((krug1[1]-krug2[1])**2+(krug1[2]-krug2[2])**2)
+                if rast < min_rast:
+                    min_rast = rast
+                    min_krug = krug2
+    return min_krug
+#--------------------------
+
+#ispis za task2
+def ispis(krugovi):
     
-    # pravim akumulator
-    width, height = gray.size
-    accumulator = [[0 for y in range(height)] for x in range(width)]
-    # Create a lookup table for sin and cos values?
-    sin_table = [sin(teta) for teta in range(360)]
-    cos_table = [cos(teta) for teta in range(360)]
-    
-    #edge detector
+    #uklanjam poluprecnik iz krugovi
+    for i in range(len(krugovi)):
+        krugovi[i] = krugovi[i][:3]
+    #----------------------------
+
+    #nalazim broj validnih logoa, pronalazim redom za boje i uklanjam ih iz circles i povecavam br
+    br_validnih = 0
+    res = []
+    while True:
+        #za svaku boju kruga zovem fju nadji
+        k1 = nadji(krugovi, 'B', 'Y')
+        k2 = nadji(krugovi, 'Y', 'B')
+        k3 = nadji(krugovi, 'Y', 'K')
+        k4 = nadji(krugovi, 'K', 'G')
+        k5 = nadji(krugovi, 'G', 'R')
+
+        if k1 == 0 or k2 == 0 or k3 == 0 or k4 == 0 or k5 == 0: 
+            break
+
+        krugovi.remove(k1)
+        krugovi.remove(k2)
+        krugovi.remove(k3)
+        krugovi.remove(k4)
+        krugovi.remove(k5)
+        
+        res.append(k1)
+        res.append(k2)
+        res.append(k3)
+        res.append(k4)
+        res.append(k5)
+
+        br_validnih += 1
+    #----------------------------
+
+    #ispis task2
+    print(br_validnih)
+    for i in res:
+        print(i[0], i[1], i[2])
+    #----------------------------
+
+#hough_circles algoritam za trazenje centrova krugova
+def hough_circles(img, rmin, rmax, steps, threshold):
+
+    #grayscale, edge detector
+    gray = img.convert('L')
     pom = np.where(np.array(gray) > 240, 255, 0)
     gray.putdata(pom.flatten())
-    # gray.show()
+    #---------------------------
 
-    #ide kroz sliku i gleda ako pixel nije crn
-    for x in range(width):
-        for y in range(height):
-            if gray.getpixel((x, y)) == 0: 
-                for teta in range(360):
-                    a = int(x - cos_table[teta])
-                    b = int(y - sin_table[teta])
-                    if a >= 0 and a < width and b >= 0 and b < height:
-                        accumulator[a][b] += 1
+    #nadji koordinate svih crnih pixela
+    pixels = np.asarray(gray)
+    koordinate = np.column_stack(np.where(pixels < 10))
+    koordinate = koordinate.tolist()
+    #---------------------------
+    
+    #nije mi najjasnije sta se nalazi u points
+    points = []
+    for r in range(rmin, rmax + 1):
+        for t in range(steps):
+            points.append((r, int(r * cos(2 * pi * t / steps)), int(r * sin(2 * pi * t / steps))))
+    #---------------------------
 
-    # najgusci presek novonastalih krugova je centar?
-    centers = []
-    for x in range(width):
-        for y in range(height):
-            if accumulator[x][y] >= threshold:
-                centers.append((x, y))
+    #nije mi ni najjasnije sta se ovde radi popunjava se direktorijum? sa 
+    acc = defaultdict(int)
+    for y, x in koordinate:
+        for r, dx, dy in points:
+            a = x - dx
+            b = y - dy
+            acc[(a, b, r)] += 1
+    #---------------------------
 
-    #output slika
-    output = Image.new('RGB', image.size, (255, 255, 255))
-    draw = ImageDraw.Draw(output)
-    for center in centers:
-        draw.ellipse((center[0]-radius, center[1]-radius, center[0]+radius, center[1]+radius), outline=(0, 0, 0))
-    output.save("da.png")
+    #nije mi ni ovo najjasnije, ovde popunjavamo circles sa bojom, koordinatama centrova i poluprecnikom
+    circles = []
+    for k, v in sorted(acc.items(), key=lambda i: -i[1]):
+        x, y, r = k
+        if v / steps >= threshold and all((x - xc) ** 2 + (y - yc) ** 2 > rc ** 2 for _, xc, yc, rc in circles):     
+            #uzimamo rgb vrednosti iz slike da bi upisali boju
+            rgb_im = img.convert('RGB')
+            rgb = rgb_im.getpixel((x + r, y))
+            if rgb in boje:
+                circles.append((boje[rgb], x, y, r))
+    #----------------------------
+
+    #pozivam ispis
+    ispis(circles)
     
 
+#main
 
 path = input()
 img = Image.open(path)
 # img = Image.open("/home/covek/Downloads/PSIML/4204/Olympic_rings/public/set/02.png")
 task1(img)
-hough(img, 70, 184)
+hough_circles(img, 50, 89, 17, 0.4)
 
-
-
+#------------------------
